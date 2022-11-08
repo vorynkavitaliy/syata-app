ecfd0e559a95b58660fde5bf3b1f6835062a44d8
Vitaliy <vorynkavetal@gmail.com>
2022-11-08 21:28:04 +0200

    start

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import { defineConfig, loadEnv } from 'vite'
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
     return {
+        base: '/dist/',
+        mode: 'production',
+        build: {
+            outDir: 'dist',
+        },
         server: {
             host: env.HOST || 'localhost',
             port: env.PORT || '4200',
-        }
+        },
     }
 })
